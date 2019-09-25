4926f18f57741514304032d3fa04d71fbc95e285
Walter_Porzel <walterp@vt.edu>
2019-09-25 10:20:26 -0400

    added CRUD

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -5,5 +5,23 @@ module.exports = {
         Model.find({}).then(data =>{
             res.json(data);
         })
+    },
+    show: (req,res) => {
+        Model.findOne({name: req.params.name}).then(country =>
+            res.json(country));
+    },
+    create: (req,res) => {
+        const newCountry = req.body;
+        Model.create(newCountry).then(country =>
+            res.json(country));
+    },
+    update: (req,res) => {
+        const updatedCountry = req.body;
+        Model.findOneAndUpdate({name: req.params.name},updatedCountry, {new: true})
+        .then(country => res.json(country));
+    },
+    destroy: (req,res) => {
+        Model.findOneAndDelete({name: req.params.name})
+        .then(country => res.json(country))
     }
 }
--- routes/Route.js
+++ routes/Route.js
@@ -4,5 +4,9 @@ const Router = express.Router();
 const Controller = require('../controllers/Controller');
 
 Router.get("/",Controller.index);
+Router.get("/:name",Controller.show);
+Router.post("/",Controller.create);
+Router.put('/:name',Controller.update);
+Router.delete('/:name',Controller.destroy);
 
 module.exports = Router;
